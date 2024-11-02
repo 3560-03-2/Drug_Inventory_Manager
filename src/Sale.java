1036a811040a1d3a8d5fe429e9c0d7fd4d3c7f91
Harry35P <168652006+Harry35P@users.noreply.github.com>
2024-11-01 22:34:29 -0700

    Update indentation

--- src/Sale.java
+++ src/Sale.java
@@ -1,7 +1,7 @@
 public class Sale {
     
     private int saleID; // Primary key
-	private Date date;	// Use the Date class of java.util package for date's type
+    private Date date;	// Use the Date class of java.util package for date's type
     private String items;
     private String receipt;
     private String customer;
