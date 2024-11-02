5d835408fc9bd1e8d8ba3488086ba1c28239d03e
CWilliams-2 <112888440+CWilliams-2@users.noreply.github.com>
2024-11-01 20:58:41 -0700

    Renamed file for use cases

---
+++ src/Methods.java
@@ -0,0 +1,23 @@
+public class Methods {
+    
+    // use case for logging a sale
+    public static void logSale(){
+
+    }
+
+    // use case for logging a delivery
+    public static void logDelivery(){
+
+    }
+
+    // use case for logging a return
+    public static void logReturn(){
+
+    }
+
+    // use case for finding a drug
+    public static void findDrug(){
+
+    }
+
+}
